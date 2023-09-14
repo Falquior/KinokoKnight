17c09e2afb6e713b3061ce36cb9198d3deddfd7d
Batman-exe <s.tangarife@uniandes.edu.co>
2023-09-14 15:50:37 -0500

    Dash

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [Tooltip("Speed of the character movement.")]
     [SerializeField] private float moveSpeed = 500.0f;
     private Vector2 moveInput;
     private Rigidbody2D rb;
+
+    // is the character using the dash?
+    private bool activeDash = false;
+    // Is the dash ready for usage?
+    private bool dashAvailable = true;
+    [Tooltip("Speed of the character when the dash is used.")]
+    [SerializeField] private float dashSpeed = 1000.0f;
+    [Tooltip("Duration in seconds of the dash.")]
+    [SerializeField] private float dashDuration = 2.0f;
+    [Tooltip("Cooldown in seconds of the dash.")]
+    [SerializeField] private float dashCooldown = 10.0f;
 
     private void Start()
     {
@@ -20,13 +32,33 @@ public class CharacterMovement : MonoBehaviour
         float verticalInput = Input.GetAxisRaw("Vertical");
         
         moveInput = new Vector2(horizontalInput, verticalInput).normalized;
+        if(Input.GetKeyDown(KeyCode.Space) && dashAvailable)
+        {
+            dashAvailable = false;
+            activeDash = true;
+            StartCoroutine(Dash());
+        }
     }
 
     private void FixedUpdate()
     {
         // Moves the character
-        rb.velocity = moveInput * moveSpeed * Time.fixedDeltaTime;
+        if(!activeDash)
+            rb.velocity = moveInput * moveSpeed * Time.fixedDeltaTime;
+        else
+            rb.velocity = moveInput * dashSpeed * Time.fixedDeltaTime;
+
     }
 
+    private IEnumerator Dash()
+    {
+        Debug.Log("Dash start");
+        yield return new WaitForSeconds(dashDuration);
+        activeDash = false;
+        Debug.Log("Dash end, cooldown");
+        yield return new WaitForSeconds(dashCooldown);
+        dashAvailable = true;
+        Debug.Log("Dash restored");
+    }
 
 }
